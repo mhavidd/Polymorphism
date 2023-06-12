d12ef9413e870a9b838c14854404a38e0fd45023
mhavidd <muhammadhavid04@students.amikom.ac.id>
2023-06-13 00:41:48 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace tes_vscode;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Pilih printer:");
+        Console.WriteLine("1. Epson");
+        Console.WriteLine("2. Canon");
+        Console.WriteLine("3. LaserJet");
+        Console.WriteLine("PILIH JENIS PRINT : (1-3)");  int input = Convert.ToInt32(Console.ReadLine());
+
+        Printer printer;
+
+        if (input == 1)
+        {
+            printer = new EpsonPrinter();
+        }
+        else if (input == 2)
+        {
+            printer = new CanonPrinter();
+        }
+        else if (input == 3)
+        {
+            printer = new LaserJetPrinter();
+        }
+        else
+        {
+            Console.WriteLine("Pilihan printer tidak valid");
+            return;
+        }
+
+        printer.DisplayDimension();
+        printer.Print();
+
+        Console.ReadLine();
+    }
+}
---
+++ printer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace tes_vscode;
+
+class Printer
+{
+    public virtual void DisplayDimension()
+    {
+        Console.WriteLine("Default display dimension");
+    }
+
+    public virtual void Print()
+    {
+        Console.WriteLine("Default printing");
+    }
+}
+
+class EpsonPrinter : Printer
+{
+    public override void DisplayDimension()
+    {
+        Console.WriteLine("Epson display dimension : 10*11");
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Epson printer printing.....");
+    }
+}
+
+class CanonPrinter : Printer
+{
+    public override void DisplayDimension()
+    {
+        Console.WriteLine("Canon display dimension : 9.5*12");
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("Canon printer printing......");
+    }
+}
+
+class LaserJetPrinter : Printer
+{
+    public override void DisplayDimension()
+    {
+        Console.WriteLine("LaserJet display dimension : 12*12");
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine("LaserJet printer printing.....");
+    }
+}
